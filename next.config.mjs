bb49b3dc06ecc01332f436ba91c711df8b951f9c
etoshutka <cheatmaster114@gmail.com>
2024-10-31 10:11:07 +0300

    vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,33 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Базовые настройки изображений
   images: {
     formats: ['image/avif', 'image/webp'],
-    domains: [
-      's2.coinmarketcap.com', 
-      'cache.tonapi.io', 
-      'crypto-swap-test.ru', 
-      'www.crypto-swap-test.ru'
-    ],
-    // Включаем встроенную оптимизацию Vercel
-    unoptimized: false,
+    domains: ['s2.coinmarketcap.com', 'cache.tonapi.io', 'crypto-swap-test.ru', 'www.crypto-swap-test.ru'],
   },
-  //assetPrefix: process.env.NODE_ENV === 'production' ? '/swap-tg-client' : '',
-  //basePath: process.env.NODE_ENV === 'production' ? '/swap-tg-client' : '',
-  
-  // Оптимизации для production
-  experimental: {
-    // Оптимизация CSS
-    optimizeCss: true,
-    // Улучшение производительности сборки
-    turbo: true,
-  },
-
-  // Настройки для production среды
-  productionBrowserSourceMaps: false, // Отключаем source maps в production
-  compress: true, // Включаем сжатие
-  poweredByHeader: false, // Отключаем заголовок X-Powered-By
-  reactStrictMode: true, // Включаем строгий режим React
-}
+};
 
 export default nextConfig;
