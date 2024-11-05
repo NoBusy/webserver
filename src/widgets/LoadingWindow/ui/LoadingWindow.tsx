66dbe950ae86315606ea05d2abeb5d17722590a1
etoshutka <cheatmaster114@gmail.com>
2024-11-05 14:10:35 +0300

    stories

--- src/widgets/LoadingWindow/ui/LoadingWindow.tsx
+++ src/widgets/LoadingWindow/ui/LoadingWindow.tsx
@@ -1,15 +1,10 @@
 'use client';
-
 import { getIsGlobalLoading, getWindowsOpen, GlobalWindow, GlobalWindowType } from '@/entities/Global';
 import { AnimatePresence, motion } from 'framer-motion';
 import styles from './LoadingWindow.module.scss';
 import { Flex } from '@/shared/ui/Flex/Flex';
 import { useSelector } from 'react-redux';
 import React, { useEffect } from 'react';
-import Image from 'next/image';
-import Yo from '@/shared/assets/icons/Yo.svg'
-import Oy from '@/shared/assets/icons/Oy.svg'
-import GradientSpinner from '@/shared/ui/GradientSpinner/GradientSpinner';
 import DotSpinner from '@/shared/ui/DotSpinner/DotSpinner';
 import { Typography } from '@/shared/ui/Typography/Typography';
 
@@ -20,7 +15,6 @@ export const LoadingWindow = () => {
 
   useEffect(() => {
     let timer: NodeJS.Timeout;
-
     if (!isGlobalLoading) {
       timer = setTimeout(() => {
         setZIndex(-1);
@@ -28,7 +22,6 @@ export const LoadingWindow = () => {
     } else {
       setZIndex(10000);
     }
-
     return () => {
       clearTimeout(timer);
     };
@@ -42,11 +35,14 @@ export const LoadingWindow = () => {
         transition={{ ease: [0.32, 0.72, 0, 1], duration: 0.6 }}
         className={styles.loading_window}
       >
-        <Flex direction="column" align="center" gap={12}>
-          {/* <Image src={Yo} alt='' width={125} height={88}/>
-          <Image src={Oy} alt='' width={125} height={88}/> */}
-          <DotSpinner />
-          <Typography.Text text = 'Loading...' type = 'secondary'/>
+        <Flex direction="column" align="center" gap={16} style={{ transform: 'translateY(-40px)' }}>
+          <div style={{ height: '52px' }}>
+            <DotSpinner size='lg' />
+          </div>
+          <Typography.Text 
+            text='Loading...' 
+            type='secondary'
+          />
         </Flex>
       </motion.div>
     </AnimatePresence>
