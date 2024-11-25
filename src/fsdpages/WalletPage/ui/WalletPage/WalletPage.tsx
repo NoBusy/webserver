43e6d9d1c8b118bc74873742b15bf8a81689f05c
etoshutka <cheatmaster114@gmail.com>
2024-11-25 16:44:25 +0300

    loading

--- src/fsdpages/WalletPage/ui/WalletPage/WalletPage.tsx
+++ src/fsdpages/WalletPage/ui/WalletPage/WalletPage.tsx
@@ -1,69 +1,16 @@
 'use client';
-import { useWalletPageLogic } from '../../lib/hooks/useWalletPageLogic';
-import { WalletPageActions } from '../WalletPageActions/WalletPageActions';
-import { WalletPageHeader } from '../WalletPageHeader/WalletPageHeader';
-import { WalletPageTokens } from '../WalletPageTokens/WalletPageTokens';
-import { WalletPageInfo } from '../WalletPageInfo/WalletPageInfo';
-import { Page } from '@/shared/ui/Page/Page';
-import { TransactionDetailsWindow } from '@/widgets/TransactionDetailsWindow';
-import { TransactionsHistoryWindow } from '@/widgets/TransactionsHistoryWindow';
-import { WalletsListWindow } from '@/widgets/WalletsListWindow';
-import { WalletDetailsWindow } from '@/widgets/WalletDetailsWindow';
-import { ImportWalletWindow } from '@/widgets/AddWalletWindow';
-import { CreateWalletWindow } from '@/widgets/AddWalletWindow';
-import { NetworksWindow } from '@/widgets/NetworksWindow';
-import { AddTokenWindow } from '@/widgets/AddTokenWindow';
-import { AddWalletWindow } from '@/widgets/AddWalletWindow';
+import dynamic from 'next/dynamic';
 import { LoadingWindow } from '@/widgets/LoadingWindow';
-import { TransferWindow } from '@/widgets/TransferWindow';
-import { DepositWindow } from '@/widgets/DepositWindow';
-import { RefWindow } from '@/widgets/RefWindow';
-import { SwapWindow } from '@/widgets/SwapWindow';
-import { TokenDetailsWindow } from '../WalletPageToken/TokenDetailsWindow';
-import { StoryViewer } from '@/widgets/StoryViewer';
-import { useEffect, useState } from 'react';
+
+// Динамически импортируем основной контент
+const WalletPageContent = dynamic(
+  () => import('./WalletPageContent'),
+  {
+    loading: () => <LoadingWindow />,
+    ssr: false // Отключаем SSR для предотвращения мелькания
+  }
+);
 
 export const WalletPage = () => {
-  const { state } = useWalletPageLogic();
-  const [isMounted, setIsMounted] = useState(false);
-
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
-
- 
-  if (!isMounted) {
-    return <div suppressHydrationWarning><LoadingWindow /></div>;
-  }
-
-
-  if (state.isLoading) {
-    return <LoadingWindow />;
-  }
-
-  return (
-    <Page>
-      <WalletPageHeader />
-      <WalletPageInfo />
-      <WalletPageTokens />
-      <WalletPageActions />
-      <TransactionsHistoryWindow />
-      <SwapWindow />
-      <DepositWindow />
-      <RefWindow />
-      <TransferWindow />
-      <NetworksWindow />
-      <LoadingWindow />
-      <AddWalletWindow />
-      <AddTokenWindow />
-      <TransactionsHistoryWindow />
-      <WalletDetailsWindow />
-      <WalletsListWindow />
-      <TransactionDetailsWindow />
-      <ImportWalletWindow />
-      <CreateWalletWindow />
-      <TokenDetailsWindow/>
-      <StoryViewer/>
-    </Page>
-  );
+  return <WalletPageContent />;
 };
