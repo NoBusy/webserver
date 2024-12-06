4fde6ea819ba4604a6f6f9ac31d3e540969236a1
etoshutka <cheatmaster114@gmail.com>
2024-12-06 17:10:00 +0300

    load pls

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 'use client';
 import { useWalletPageLogic } from '@/fsdpages/WalletPage/lib/hooks/useWalletPageLogic';
-import { LoadingWindow } from '@/widgets/LoadingWindow';
 import dynamic from 'next/dynamic';
 
 const InitializedApp = dynamic(
   () => import('@/fsdpages/WalletPage/ui/WalletPage/WalletPage'), 
   {
-    // ssr: false,
-    loading: () => <LoadingWindow />
+    loading: () => null
   }
 );
 
