34a85144a5eb6af316e9dffd4d7ff1be2e1053d3
etoshutka <cheatmaster114@gmail.com>
2024-12-06 17:15:25 +0300

    del dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,11 @@
 'use client';
+import WalletPage from '@/fsdpages/WalletPage/ui/WalletPage/WalletPage';
 import { useWalletPageLogic } from '@/fsdpages/WalletPage/lib/hooks/useWalletPageLogic';
-import dynamic from 'next/dynamic';
-
-const InitializedApp = dynamic(
-  () => import('@/fsdpages/WalletPage/ui/WalletPage/WalletPage'), 
-  {
-    loading: () => null
-  }
-);
 
 export default function NextJsPage() {
   const { state } = useWalletPageLogic();
   
   if (!state) return null;
-
-  return <InitializedApp state={state} />; // Передаем состояние
+  
+  return <WalletPage state={state} />;
 }
