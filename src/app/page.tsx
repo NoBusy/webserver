0188a3ea0047dd291a1cd062dea9f2f9d90432b6
etoshutka <cheatmaster114@gmail.com>
2024-12-06 17:37:27 +0300

    s

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,11 @@
 'use client';
 import WalletPage from '@/fsdpages/WalletPage/ui/WalletPage/WalletPage';
 import { useWalletPageLogic } from '@/fsdpages/WalletPage/lib/hooks/useWalletPageLogic';
-import { LoadingWindow } from '@/widgets/LoadingWindow';
-import { useEffect, useState } from 'react';
 
 export default function NextJsPage() {
   const { state } = useWalletPageLogic();
-  const [shouldRender, setShouldRender] = useState(false);
   
-  useEffect(() => {
-    if (state) {
-      // Используем RAF для синхронизации с браузерным рендерингом
-      requestAnimationFrame(() => {
-        setShouldRender(true);
-      });
-    }
-  }, [state]);
-
-  // Показываем loading пока state не готов
-  if (!shouldRender) {
-    return <LoadingWindow />;
-  }
+  if (!state) return null;
   
   return <WalletPage state={state} />;
 }
