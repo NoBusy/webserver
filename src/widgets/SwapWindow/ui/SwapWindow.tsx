29a3f91df61f544ab4f5f8f939bbdcb19727cef4
etoshutka <cheatmaster114@gmail.com>
2024-12-10 16:29:15 +0300

    add link

--- src/widgets/SwapWindow/ui/SwapWindow.tsx
+++ src/widgets/SwapWindow/ui/SwapWindow.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { UseSwapWindowLogic, useSwapWindowLogic } from '../lib/hooks/useSwapWindowLogic';
 import { WindowHeader } from '@/shared/ui/Header/WindowHeader';
 import { Window } from '@/shared/ui/Window/Window';
 import { PrepareSwapWindow } from './PrepareSwapWindow';
 import { ConfirmSwapWindow } from './ConfirmSwapWindow';
 import { SelectTokenPage } from './SelectTokenModal';
+import { useSelector } from 'react-redux';
+import { GlobalWindow, getWindowsOpen } from '@/entities/Global';
 
 export const SwapWindow: React.FC = () => {
   const logic: UseSwapWindowLogic = useSwapWindowLogic();
   const { state, flow } = logic;
+  const openedWindows = useSelector(getWindowsOpen);
+
+  useEffect(() => {
+    const swapWindow = openedWindows.find(w => w.window === GlobalWindow.Swap);
+    if (swapWindow?.options?.params) {
+      const { fromToken, toToken, network } = swapWindow.options.params;
+      flow.setInitialTokens({ fromToken, toToken, network });
+    }
+  }, [openedWindows]);
 
   return (
     <>
@@ -16,22 +27,21 @@ export const SwapWindow: React.FC = () => {
         <WindowHeader title="Swap" />
         <PrepareSwapWindow logic={logic} />
       </Window>
-
       <SelectTokenPage
         tokens={state.currentView === 'selectFromToken' ? state.fromTokens : state.toTokens}
-        onSelectToken={state.currentView === 'selectFromToken' 
-          ? flow.handleSelectFromToken 
+        onSelectToken={state.currentView === 'selectFromToken'
+          ? flow.handleSelectFromToken
           : flow.handleSelectToToken
         }
-        title={state.currentView === 'selectFromToken' 
-          ? "Select token to swap from" 
+        title={state.currentView === 'selectFromToken'
+          ? "Select token to swap from"
           : "Select token to swap to"
         }
         isOpen={state.currentView === 'selectFromToken' || state.currentView === 'selectToToken'}
         onClose={flow.handleBackToSwap}
       />
-
-      {state.isConfirmSwapWindowOpen && <ConfirmSwapWindow logic={logic} onClose={flow.handleClearState} />}
+      {state.isConfirmSwapWindowOpen && 
+        <ConfirmSwapWindow logic={logic} onClose={flow.handleClearState} />}
     </>
   );
 };
